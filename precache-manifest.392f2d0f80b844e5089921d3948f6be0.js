de75736712f74e714db77d7ac6f421af168d0f3e
Volodymyr Kryvytskyy <volodymyrkryvytskyy@MacBook-Pro.mate.academy>
2019-06-27 20:29:51 +0300

    Updates

--- precache-manifest.392f2d0f80b844e5089921d3948f6be0.js
+++ precache-manifest.392f2d0f80b844e5089921d3948f6be0.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "42f2e019389e8d65b753cb1549310d3a",
+    "revision": "480255373d16510c510c1ebf5bbcaa34",
     "url": "/react_airport/index.html"
   },
   {
-    "revision": "28622a3d531adf381191",
+    "revision": "ce57e4c56d18a5d066aa",
     "url": "/react_airport/static/css/main.17524c7e.chunk.css"
   },
   {
-    "revision": "2315f74ce3c99316133d",
-    "url": "/react_airport/static/js/2.3310f33a.chunk.js"
+    "revision": "489ffaf5c08063324789",
+    "url": "/react_airport/static/js/2.29a7cb42.chunk.js"
   },
   {
-    "revision": "28622a3d531adf381191",
-    "url": "/react_airport/static/js/main.a9d7a70a.chunk.js"
+    "revision": "ce57e4c56d18a5d066aa",
+    "url": "/react_airport/static/js/main.c9d67da8.chunk.js"
   },
   {
     "revision": "c8cd8e70b1c2d1cd9765",
