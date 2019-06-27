a093aef3de3898f9d2624dc10503428f5184b14c
Volodymyr Kryvytskyy <volodymyrkryvytskyy@MacBook-Pro.mate.academy>
2019-06-27 20:29:08 +0300

    Working version without styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Arrivals from "./Arrivals";
+import Departures from "./Departures";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      currentPage: 'Arrivals'
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  
+
+  handleClick(event) {
+    event.persist();
+    this.setState({currentPage: event.target.textContent})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.handleClick}>Departures</button>
+        <button onClick={this.handleClick}>Arrivals</button>
+        <table className="AppTable">
+          <thead>
+            <tr>
+              <th>Terminal</th>
+              <th>Local Time</th>
+              <th>Destination</th>
+              <th>Status</th>
+              <th>Airline</th>
+              <th>Flight</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.currentPage === 'Arrivals' ? <Arrivals /> : <Departures />}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Arrivals.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+class Arrivals extends Component {
+  constructor(props) {
+    super(props)
+    this.getData()
+    this.state = {
+      arrivals: []
+    }
+  }
+
+  async getData() {
+    const currentDate = new Date().toLocaleDateString('ru-RU').replace(/[^0-9]/gi, '-');
+    const url = `https://api.iev.aero/api/flights/${currentDate}`
+    const dataPromise = await fetch(url);
+    const fetchedData = await dataPromise.json();
+    this.setState({
+      arrivals : [...fetchedData.body.arrival]
+    })
+  }
+
+  render() {
+    const arrivalsData = [...this.state.arrivals]
+      .map(flight => {
+
+        const localTime = new Date(flight.timeArrShedule)
+          .toTimeString()
+          .match(/\b[0-1][0-9]:[0-5][0-9]\b|\b2[0-3]:[0-5][0-9]\b/);
+      return (
+        <tr className="arrivals">
+          <td>{flight.term}</td>
+          <td>{localTime}</td>
+          <td>{flight["airportFromID.city"]}</td>
+          <td>{flight.status}</td>
+          <td>{flight.airline.ua.name}</td>
+          <td>{flight['codeShareData']['0']['icao']+flight.fltNo}</td>
+        </tr>
+      )
+      })
+    return (
+      arrivalsData
+    )
+  }
+}
+
+export default Arrivals
