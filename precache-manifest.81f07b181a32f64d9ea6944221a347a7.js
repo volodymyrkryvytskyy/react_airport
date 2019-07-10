d3a1965c97d0d4d6c632d0b4bcb9520d7694b903
Volodymyr Kryvytskyy <volodymyrkryvytskyy@Volodymyrs-MBP.mate.academy>
2019-07-10 11:58:01 +0300

    Updates

--- precache-manifest.81f07b181a32f64d9ea6944221a347a7.js
+++ precache-manifest.81f07b181a32f64d9ea6944221a347a7.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "48093b088589162e89dbe2d2e3bb6b45",
+    "revision": "41ed68ab38cdd7dcdae9d80f63379ac9",
     "url": "/react_airport/index.html"
   },
   {
-    "revision": "79911b4e0442224c11a0",
+    "revision": "31516252652c50c9e3b9",
     "url": "/react_airport/static/css/main.254ef553.chunk.css"
   },
   {
-    "revision": "5452be9e1a653f4c4568",
-    "url": "/react_airport/static/js/2.4b73179b.chunk.js"
+    "revision": "4075caab9953b5dc1ab4",
+    "url": "/react_airport/static/js/2.acff2ca5.chunk.js"
   },
   {
-    "revision": "79911b4e0442224c11a0",
-    "url": "/react_airport/static/js/main.8950c229.chunk.js"
+    "revision": "31516252652c50c9e3b9",
+    "url": "/react_airport/static/js/main.3187324f.chunk.js"
   },
   {
     "revision": "c8cd8e70b1c2d1cd9765",
